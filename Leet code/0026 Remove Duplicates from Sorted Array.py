bcccf2494e9426e9630f9d5846484381679458e5
Dalyaeva <arinadalaeva@gmail.com>
2026-04-15 13:07:07 +0300

    all готовая 0026 задача

--- Leet code/0026 Remove Duplicates from Sorted Array.py
+++ Leet code/0026 Remove Duplicates from Sorted Array.py
@@ -2,13 +2,19 @@ nums = [0,0,1,1,1,2,2,3,3,4]
 
 
 def removeDuplicates(nums):
-    k = 1
+    k = 0
     for i in range (len(nums)-1):
-        if nums[i] != nums[i+1]:
+        if nums[i] == nums[i+1]:
+            continue
+        else:
             k = k + 1
-    for i in range (k, len(nums, 1)):
+            nums[k] = nums[i+1]
+            print(k, i)
+    k = k + 1
+    for i in range (k, len(nums),1):
         nums[i] = '_'
     return k
 
 
 print(removeDuplicates(nums))
+print(nums)
