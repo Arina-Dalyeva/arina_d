69396dfad035806515b207fd5d5617ab98de93e3
Dalyaeva <arinadalaeva@gmail.com>
2026-04-30 08:53:31 +0300

    all

---
+++ Leet code/0118. Pascal's Triangle.py
@@ -0,0 +1,21 @@
+m = 9
+lst = [0] * m
+
+
+def treygolnik(lst):
+    x = 0
+    for i in range(len(lst) - 1):
+        lst[0] = [1]
+        print(lst)
+        lst[i + 1] = [1] * (i + 2)
+        print(lst)
+    for i in range(len(lst)):
+        for j in range(len(lst[i])-1):
+            if len(lst[i]) > 2 and j != 0:
+                lst[i][j] = i
+    for item in lst:
+        print(item)
+    return lst
+
+
+print(treygolnik(lst))
